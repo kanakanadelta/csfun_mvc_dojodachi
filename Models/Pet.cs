2d45460c6a32bf778f7f3401f08f7bca743123a4
Rafael Tongson <rtongson@peppymiint.local>
2019-10-22 09:40:57 -0700

    initialized pet model

---
+++ Models/Pet.cs
@@ -0,0 +1,17 @@
+namespace Dojodachi.Models
+{
+    public class Pet
+    {
+        private int happiness = 20;
+        private int fullness = 20;
+        private int energy = 50;
+        private int meals = 3;
+        public int Happiness { get; set; }
+        public int Fullness { get; set; }
+        public int Energy { get; set; }
+        public int Meals { get; set; }
+
+        // methods
+        
+    }
+}
